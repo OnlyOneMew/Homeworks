f9c4f35cafb122eff9816ccc82d0183ae96739e3
OnlyOneMew <boka.gigiberiaa@gmail.com>
2024-02-28 15:58:01 +0400

    homework 2, test one done

---
+++ day-2/test-1.py
@@ -0,0 +1,8 @@
+user_input = input("Enter true or false: ").lower()
+
+if user_input == "true":
+    print("whoala")
+elif user_input == "false":
+    print("Input was false.")
+else:
+    print("Please enter either true or false.")
