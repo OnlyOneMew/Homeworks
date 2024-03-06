d9c6bc1dbfada0667322f7188ca16c5572858309
OnlyOneMew <boka.gigiberiaa@gmail.com>
2024-03-06 18:27:05 +0400

    Remove Homework-1 directory from homework-4 branch

--- Homework-1/test-1.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-last_name = input("Enter your lastname: ")
-age = int(input("Enter your age: "))
-print(f"Hello,{name} {last_name}, You are {age} years old")
--- Homework-1/test-2.py
+++
@@ -1,4 +0,0 @@
-name = input("What's your name? ")
-birth_year = int(input("Enter your birth year: "))
-current_age = 2024 - birth_year
-print(f"Hello {name}, you are {current_age} years old.")
